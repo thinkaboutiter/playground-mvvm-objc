54d129d7c5e93c9512cf3fbde3179a357ec07798
boyankov <boyan.yankov@yahoo.com>
2019-07-16 23:05:40 +0300

    added initial project

---
+++ MvvmSampleObjC/MvvmSampleObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MvvmSampleObjC
+//
+//  Created by Boyan Yankov on 2019-W29-16-Jul-Tue.
+//  Copyright © 2019 boyankov@yahoo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MvvmSampleObjC/MvvmSampleObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MvvmSampleObjC
+//
+//  Created by Boyan Yankov on 2019-W29-16-Jul-Tue.
+//  Copyright © 2019 boyankov@yahoo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
